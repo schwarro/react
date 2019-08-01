93cc766227bf1cbefd2ce05b0a4adf8f097cf32f
schwarro <rob.ap.schwartz@gmail.com>
2019-08-01 13:43:40 -0400

    worked with promises

--- es6/src/main.js
+++ es6/src/main.js
@@ -216,10 +216,70 @@
 //
 // add(2,2);
 
-let add = (a,b) => {
-  let sum = a + b;
-  console.log(sum);
-  return false;
+// let add = (a,b) => {
+//   let sum = a + b;
+//   console.log(sum);
+//   return false;
+// }
+//
+// add(2,2);
+
+//-------------------------------------------------
+
+//Promises
+
+//Immediate Resolution
+// let myPromise = Promise.resolve('Foo');
+//
+// myPromise.then((res) => console.log(res));
+
+// let myPromise = new Promise(function(resolve, reject){
+//   setTimeout(() => resolve(4), 2000);
+// });
+//
+// myPromise.then((res) => {
+//   res += 3;
+//   console.log(res);
+// });
+
+function getData(method, url){
+  return new Promise(function(resolve, reject){
+    let xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onload = function(){
+      if(this.status >= 200 && this.status < 300){
+        resolve(xhr.response);
+      } else {
+        reject({
+          status: this.status,
+          statusText: xhr.statusText
+        });
+      }
+    };
+    xhr.onerror = function(){
+      reject({
+        status: this.status,
+        statusText: xhr.statusText
+      });
+    };
+    xhr.send();
+  });
 }
 
-add(2,2);
+getData('GET', 'https://jsonplaceholder.typicode.com/todos').then(function(data){
+  // console.log(data);
+  let todos = JSON.parse(data);
+  let output = '';
+  for(let todo of todos) {
+    output += `
+    <li>
+      <h3>${todo.title}</h3>
+      <p>Completed: ${todo.completed}</p>
+    </li>
+    `;
+  }
+
+  document.getElementById('template').innerHTML = output;
+}).catch(function(err){
+  console.log(err);
+});
