1004f4da033006c91c444d63f3fc73b22d8c6894
schwarro <rob.ap.schwartz@gmail.com>
2019-08-13 17:20:49 -0400

    added nav bar

--- udemy/github-finder/src/App.js
+++ udemy/github-finder/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 // import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
-
+class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello From React</h1>
+      <div className='App'>
+        <Navbar />
       </div>
     );
   }
