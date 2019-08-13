2d0a3a19bdbc911b00a296f8e5a2f70e861780cd
schwarro <rob.ap.schwartz@gmail.com>
2019-08-13 16:44:13 -0400

    started react github finder

---
+++ udemy/github-finder/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello From React</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
