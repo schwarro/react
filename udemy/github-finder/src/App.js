1e61c235c8d23f34315ca1f3b61d1a5a4dd70b95
schwarro <rob.ap.schwartz@gmail.com>
2019-08-15 14:53:19 -0400

    finished functional site, refactoring into functional components next

--- udemy/github-finder/src/App.js
+++ udemy/github-finder/src/App.js
@@ -15,7 +15,8 @@ class App extends Component {
     users: [],
     user: {},
     loading: false,
-    alert: null
+    alert: null,
+    repos: []
   };
 
   // async componentDidMount() {
@@ -48,6 +49,17 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   }
 
+  //Get Users Repos
+  getUserRepos = async username => {
+    this.setState({ loading: true});
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ repos: res.data, loading: false });
+  }
+
   //Clear Users from State
   clearUsers = () => this.setState({ users: [], loading: false});
 
@@ -58,7 +70,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, repos, loading } = this.state;
 
     return (
       <Router>
@@ -90,7 +102,9 @@ class App extends Component {
                   <User
                     { ...props }
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
