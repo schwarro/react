357a5587b0c2f8ad1f531a08a7f70acc116f4a38
schwarro <rob.ap.schwartz@gmail.com>
2019-08-22 16:05:23 -0400

    added home and not found components

--- udemy/github-finder/src/App.js
+++ udemy/github-finder/src/App.js
@@ -1,11 +1,13 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
+// import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
+// import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import Home from './components/pages/Home';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 // import logo from './logo.svg';
 import './App.css';
 // import axios from 'axios';
@@ -90,18 +92,10 @@ const App = () => {
           <div className="container">
             <Alert />
             <Switch>
-              <Route
-              exact
-              path='/'
-              render={props => (
-                <Fragment>
-                  <Search />
-                  <Users />
-                </Fragment>
-              )}
-              />
+              <Route exact path='/' component={Home} />
               <Route exact path='/about' component={About} />
               <Route exact path='/user/:login' component={User}/>
+              <Route component ={NotFound} />
             </Switch>
           </div>
         </div>
