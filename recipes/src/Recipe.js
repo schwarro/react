227a95841a6f22cbcc921d0be455da22762758df
schwarro <rob.ap.schwartz@gmail.com>
2019-09-10 13:02:10 -0400

    styled

--- recipes/src/Recipe.js
+++ recipes/src/Recipe.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import style from './recipe.module.css'
 
 const Recipe = ({title, calories, image, ingredients}) => {
   return(
-    <div>
+    <div className={ style.recipe }>
       <h1>{title}</h1>
       <p>{calories}</p>
-      <img src={image} alt="" />
-      <ul>
+      <img className={ style.image } src={image} alt="" />
+      <ol>
         {ingredients.map(ingredient => (
           <li>{ ingredient.text }</li>
         ))}
-      </ul>
+      </ol>
     </div>
   )
 }
