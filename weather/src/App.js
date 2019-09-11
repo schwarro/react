23a76687f59d9bb2ddb3cdb0d2a247b042799839
schwarro <rob.ap.schwartz@gmail.com>
2019-09-11 19:21:49 -0400

    fixed env

--- weather/src/App.js
+++ weather/src/App.js
@@ -4,7 +4,7 @@ import Titles from './components/Titles'
 import Form from './components/Form'
 import Weather from './components/Weather'
 
-const API_KEY = process.env.REACT_API_KEY
+const API_KEY = process.env.REACT_APP_API_KEY
 
 class App extends React.Component {
   state = {
