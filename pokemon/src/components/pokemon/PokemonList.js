846d853da42f536245e783bb39c8100fe81c9f47
schwarro <rob.ap.schwartz@gmail.com>
2019-09-16 14:52:39 -0400

    added pokemon api

--- pokemon/src/components/pokemon/PokemonList.js
+++ pokemon/src/components/pokemon/PokemonList.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import PokemonCard from './PokemonCard'
-import styled from 'styled-components'
 
 export default class PokemonList extends Component {
+  state={
+    url: 'https://pokeapi.co/api/v2/pokemon/',
+    pokemon: null
+  }
+
+  async componentDidMount(){
+    const res = await axios.get(this.state.url);
+    this.setState({ pokemon: res.data['results'] });
+  }
+
   render() {
     return (
-      <div className="row">
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
-      </div>
+      <React.Fragment>
+        {this.state.pokemon ? (
+          <div className="row">
+            {this.state.pokemon.map(pokemon => (
+              <PokemonCard />
+            ))}
+          </div>
+        ) : (
+          <h1>Loading Pokemon...</h1>
+        )}
+      </React.Fragment>
     )
   }
 }
