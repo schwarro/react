7d214992c5f3fbe546f3aca38e373cf874c6ae42
schwarro <rob.ap.schwartz@gmail.com>
2019-09-16 14:11:38 -0400

    added navbar, working on pokemon cards

---
+++ pokemon/src/components/pokemon/PokemonList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styled from 'styled-components'
+
+export default class PokemonList extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
