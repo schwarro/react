20913fbc50f75bdea1a161281ba6b5bd70d89deb
schwarro <rob.ap.schwartz@gmail.com>
2019-09-17 15:53:59 -0400

    styled cards, working on background

--- pokemon/src/components/pokemon/PokemonCard.js
+++ pokemon/src/components/pokemon/PokemonCard.js
@@ -8,6 +8,20 @@ const Sprite = styled.img`
   display: none;
 `;
 
+const Card = styled.div`
+  opacity: 0.95;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+  &:hover {
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  }
+  -moz-user-select: none;
+  -website-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  -o-user-select: none;
+`;
+
   export default class PokemonCard extends Component {
     state = {
       name: '',
@@ -28,7 +42,7 @@ const Sprite = styled.img`
   render() {
     return (
       <div className="col-md-3 col-sm-6 mb-5">
-        <div className="card">
+        <Card className="card">
           <h5 className="card-header">{this.state.pokemonIndex}</h5>
 
           {this.state.imageLoading ? (
@@ -70,7 +84,7 @@ const Sprite = styled.img`
                 .join(' ')}
             </h6>
           </div>
-        </div>
+        </Card>
       </div>
     )
   }
