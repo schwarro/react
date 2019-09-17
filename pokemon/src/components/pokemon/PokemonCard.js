65afbaf7612f24d808adf996c4d1195d61f9e607
schwarro <rob.ap.schwartz@gmail.com>
2019-09-17 13:42:50 -0400

    fixed images

--- pokemon/src/components/pokemon/PokemonCard.js
+++ pokemon/src/components/pokemon/PokemonCard.js
@@ -4,38 +4,37 @@ import styled from 'styled-components'
 const Sprite = styled.img`
   width: 5em;
   height: 5em;
-
 `;
-export default class PokemonCard extends Component {
 
-  state = {
-    name: '',
-    imgUrl: '',
-    pokemonIndex: '',
-    imageLoading: true,
-    tooManyRequests: false
-  }
+  export default class PokemonCard extends Component {
+    state = {
+      name: '',
+      imageUrl: '',
+      pokemonIndex: '',
+      imageLoading: true,
+      toManyRequests: false
+    };
 
-  componentDidMount() {
-    const { name, url } = this.props;
-    const pokemonIndex = url.split('/')[url.split('/').length - 2];
-    //const imageUrl = `./sprites/pokemon/${pokemonIndex}.png`;
-    const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`;
+    componentDidMount() {
+      const { name, url } = this.props;
+      const pokemonIndex = url.split('/')[url.split('/').length - 2];
+      const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`;
 
-    this.setState({ name, imageUrl, pokemonIndex })
-  }
+      this.setState({ name, imageUrl, pokemonIndex });
+    }
 
   render() {
     return (
       <div className="col-md-3 col-sm-6 mb-5">
         <div className="card">
           <h5 className="card-header">{this.state.pokemonIndex}</h5>
+
           <Sprite
             className="card-img-top rounded mx-auto mt-2"
+            src={this.state.imageUrl}
             onLoad={() => this.setState({ imageLoading: false })}
-            onError={() => this.setState({ tooManyRequests: true })}
-            src="{this.state.imgUrl}">
-          </Sprite>
+            onError={() => this.setState({ toManyRequests: true })}
+          />
           <div className="card-body mx-auto">
             <h6 className="card-title">
               {this.state.name
