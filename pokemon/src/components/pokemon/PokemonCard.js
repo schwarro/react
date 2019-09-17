98adfcf72c57a565be5321f9e1cd4115b7ba5888
schwarro <rob.ap.schwartz@gmail.com>
2019-09-17 15:44:09 -0400

    images are good, added loading gif

--- pokemon/src/components/pokemon/PokemonCard.js
+++ pokemon/src/components/pokemon/PokemonCard.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import styled from 'styled-components'
+import spinner from '../layout/spinner.gif'
 
 const Sprite = styled.img`
   width: 5em;
   height: 5em;
+  display: none;
 `;
 
   export default class PokemonCard extends Component {
@@ -29,12 +31,36 @@ const Sprite = styled.img`
         <div className="card">
           <h5 className="card-header">{this.state.pokemonIndex}</h5>
 
+          {this.state.imageLoading ? (
+            <img
+              src={spinner}
+              style={{ width: '5em', height: '5em' }}
+              className="card-img-top rounded mx-auto d-block mt-2"
+            />
+          ) : null}
+
           <Sprite
             className="card-img-top rounded mx-auto mt-2"
             src={this.state.imageUrl}
             onLoad={() => this.setState({ imageLoading: false })}
             onError={() => this.setState({ toManyRequests: true })}
+            style = {
+              this.state.toManyRequests
+                ? { display: 'none' }
+                : this.state.imageLoading
+                ? null
+                : { display: 'block'}
+            }
           />
+
+          {this.state.toManyRequests ? (
+            <h6 className="mx-auto">
+              <span className="badge badge-danger mt-2">
+                To Many Requests
+              </span>
+            </h6>
+          ) : null}
+
           <div className="card-body mx-auto">
             <h6 className="card-title">
               {this.state.name
