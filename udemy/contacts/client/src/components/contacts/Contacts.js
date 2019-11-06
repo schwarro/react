0a40e9cbb31bd53225b45b669318dccd929a9019
schwarro <rob.ap.schwartz@gmail.com>
2019-11-06 16:45:59 -0500

    added animation

--- udemy/contacts/client/src/components/contacts/Contacts.js
+++ udemy/contacts/client/src/components/contacts/Contacts.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useContext } from 'react';
 import ContactContext from '../../context/contact/contactContext';
 import ContactItem from './ContactItem';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
@@ -13,13 +14,19 @@ const Contacts = () => {
 
   return (
     <Fragment>
-      {filtered !== null
-        ? filtered.map(contact => (
-            <ContactItem key={contact.id} contact={contact} />
-          ))
-        : contacts.map(contact => (
-            <ContactItem key={contact.id} contact={contact}/>
-          ))}
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map(contact => (
+            <CSSTransition key={contact.id} timeout={500} classNames="item">
+              <ContactItem contact={contact} />
+            </CSSTransition>
+            ))
+          : contacts.map(contact => (
+            <CSSTransition key={contact.id} timeout={500} classNames="item">
+              <ContactItem contact={contact}/>
+            </CSSTransition>
+            ))}
+      </TransitionGroup>
     </Fragment>
   );
 };
